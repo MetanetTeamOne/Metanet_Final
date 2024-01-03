5da5c5bbcd0351ff32d63087855fc9a62f26f8c0
hissue <wldmx@naver.com>
2024-01-03 16:35:15 +0900

    Feat : 코드 정리

--- finalproject/src/main/java/com/metanet/finalproject/orders/controller/OrdersController.java
+++ finalproject/src/main/java/com/metanet/finalproject/orders/controller/OrdersController.java
@@ -11,9 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.metanet.finalproject.files.model.Files;
 import com.metanet.finalproject.files.service.IFilesService;
