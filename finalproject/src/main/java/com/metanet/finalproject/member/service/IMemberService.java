e8028f7cb0833c80fe69caf0ffe8cf4e3d372968
hissue <wldmx@naver.com>
2024-01-04 22:21:52 +0900

    Feat: jwttoken 적용 중 구독, 패스워드, 회원 관리 적용 완료

--- finalproject/src/main/java/com/metanet/finalproject/member/service/IMemberService.java
+++ finalproject/src/main/java/com/metanet/finalproject/member/service/IMemberService.java
@@ -3,17 +3,17 @@ package com.metanet.finalproject.member.service;
 import java.util.List;
 
 import com.metanet.finalproject.member.model.Member;
-import com.metanet.finalproject.member.model.MemberUpdateDto;
 
 public interface IMemberService {
     public Member getMember(int memberId);
     public List<Member> getMemberList();
     public void insertMember(Member member);
     public void updateMember(Member member, String email);
-    public void deleteMember(String password);
+    public void deleteMember(String memberEmail, String memberPassword);
 	public int getMemberId(String email);
     //이메일로 특정 회원조회
   	Member selectMember(String memberEmail);
+
     // 구독신청 -> 구독o : 1
  	void insertSubscribe(Member member);
  	// 구독변경 -> 구독x : 0, 
