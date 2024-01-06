d07c7f3537f0d51508d12e180b86d6013ff6b061
hissue <wldmx@naver.com>
2024-01-07 00:24:39 +0900

    Feat: 충돌 해제 완료

--- finalproject/src/main/java/com/metanet/finalproject/HomeController.java
+++ finalproject/src/main/java/com/metanet/finalproject/HomeController.java
@@ -9,24 +9,18 @@ public class HomeController {
 	// 홈
 	@GetMapping("/")
 	public String home() {
-		System.out.println("home");
 		return "member/home";
 	}
-	
-
-
 
 	// 서비스 안내
 	@GetMapping("/svcinfo")
 	public String serviceInformaion() {
-		System.out.println("svcinfo");
 		return "member/service_information";
 	}
 	
 	// 이용 방법
 	@GetMapping("/howuse")
 	public String howUse() {
-		System.out.println("howuse");
 		return "member/how_use";
 	}
 	
@@ -34,49 +28,42 @@ public class HomeController {
 	// 관리자 홈
 	@GetMapping("/admin")
 	public String adminHome() {
-		System.out.println("admin");
 		return "admin/adminHome";
 	}
 	
 	// 회원 관리
 	@GetMapping("/admin/member")
 	public String adminMember() {
-		System.out.println("adminMember");
 		return "admin/adminMember";
 	}
 	
 	// 게시판 관리
 	@GetMapping("/admin/board")
 	public String adminBoard() {
-		System.out.println("adminBoard");
 		return "admin/adminBoard";
 	}
 	
 	// 세탁물 관리
 	@GetMapping("/admin/laundry")
 	public String adminLaundry() {
-		System.out.println("adminLaundry");
 		return "admin/adminLaundry";
 	}
 	
 	// 주문 관리
 	@GetMapping("/admin/order")
 	public String adminOrder() {
-		System.out.println("adminOrder");
 		return "admin/adminOrder";
 	}
 	
 	// 결제 관리
 	@GetMapping("/admin/pay")
 	public String getPay() {
-		System.out.println("getPay");
 		return "admin/adminPay";
 	}
 	
 	// 매출 관리
 	@GetMapping("/admin/chart")
 	public String getCharts() {
-		System.out.println("getCharts");
 		return "admin/adminChart";
 	}
 
