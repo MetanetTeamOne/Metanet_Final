6a68dc5e175da24dbf02803c83dbbd68544c133f
hissue <wldmx@naver.com>
2024-01-06 21:43:02 +0900

    Feat: swagger 활성화

--- finalproject/src/main/java/com/metanet/finalproject/config/SwaggerConfig.java
+++ finalproject/src/main/java/com/metanet/finalproject/config/SwaggerConfig.java
@@ -1,24 +1,24 @@
-//package com.metanet.finalproject.config;
-//
-//import io.swagger.v3.oas.models.Components;
-//import io.swagger.v3.oas.models.OpenAPI;
-//import io.swagger.v3.oas.models.info.Info;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-// 
-//@Configuration
-//public class SwaggerConfig {
-//    @Bean
-//    public OpenAPI openAPI() {
-//        return new OpenAPI()
-//                .components(new Components())
-//                .info(apiInfo());
-//    }
-// 
-//    private Info apiInfo() {
-//        return new Info()
-//                .title("메타넷 클리닝 서비스")
-//                .description("서비스 API 테스트")
-//                .version("1.0.0");
-//    }
-//}
+package com.metanet.finalproject.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+ 
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+ 
+    private Info apiInfo() {
+        return new Info()
+                .title("메타넷 클리닝 서비스")
+                .description("서비스 API 테스트")
+                .version("1.0.0");
+    }
+}
