d72c6696447bc218eef839370841622751ccbd9d
maru6889 <crazyboy114@naver.com>
2024-01-07 15:45:02 +0900

    회원 삭제 검증 추가

---
+++ finalproject/src/main/java/com/metanet/finalproject/member/model/MemberDeleteDto.java
@@ -0,0 +1,9 @@
+package com.metanet.finalproject.member.model;
+
+import lombok.Data;
+
+@Data
+public class MemberDeleteDto {
+    private String memberEmail;
+    private String memberPassword;
+}
