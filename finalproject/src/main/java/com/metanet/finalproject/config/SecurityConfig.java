b7382e9b3d7ec0895a2bb20c0e33c4618fcd1a82
maru6889 <crazyboy114@naver.com>
2024-01-09 10:15:44 +0900

    .

--- finalproject/src/main/java/com/metanet/finalproject/config/SecurityConfig.java
+++ finalproject/src/main/java/com/metanet/finalproject/config/SecurityConfig.java
@@ -77,6 +77,7 @@ public class SecurityConfig {
       // 토큰을 사용하는 경우 인가를 적용한 URI 설정
       http.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
 //              .requestMatchers("/file/**").hasRole("ADMIN")
+              .requestMatchers("/member").permitAll()
               .requestMatchers("/member/**").hasAnyRole("USER", "ADMIN")
               .requestMatchers("/**", "/css/**", "/js/**", "/images/**").permitAll()
               .requestMatchers("/v3/api-docs/**","/swagger-ui/**").permitAll()
