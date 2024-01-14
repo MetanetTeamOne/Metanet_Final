34b97d4cd60c0b7f6aacf4c28ec1c4fe0f75f5b2
hissue <wldmx@naver.com>
2024-01-14 11:59:45 +0900

    Feat : 직접 결제 구현 최종 완료

--- finalproject/src/main/resources/static/js/member/directPay.js
+++ finalproject/src/main/resources/static/js/member/directPay.js
@@ -1,7 +1,7 @@
 
 function directPay (price, count, items, orderId) {
   BootPay.request({
-      price: 101, //실제 결제되는 가격
+      price: price, //실제 결제되는 가격
  
       // 관리자로그인 -> 결제설치 -> 인증키 및 보안 -> WEB Application ID
       application_id: "65a160cc00c78a001d3460d7",
@@ -14,9 +14,11 @@ function directPay (price, count, items, orderId) {
       order_id: orderId, //고유 주문번호로, 생성하신 값을 보내주셔야 합니다.
   }).error(function (data) {
       //결제 진행시 에러가 발생하면 수행됩니다.
+      location.reload();
       console.log("에러 발생 : ", data);
   }).cancel(function (data) {
       //결제가 취소되면 수행됩니다.
+      location.reload();
       console.log("결제 취소 : ", data);
   }).close(function (data) {
       // 결제창이 닫힐때 수행됩니다. (성공,실패,취소에 상관없이 모두 수행됨)
